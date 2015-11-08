1660208e5e74f24847f477d8c82d5581f035b076
Andreas Treichel <gmblar+github@gmail.com>
2015-11-08 15:06:50 +0100

    Update Unittests

--- tests/Blar/Hash/HmacHashTest.php
+++ tests/Blar/Hash/HmacHashTest.php
@@ -11,21 +11,22 @@ use PHPUnit_Framework_TestCase as TestCase;
 class HmacHashTest extends TestCase {
 
     public function testMd5() {
-        $hash = new HmacHash('md5', 1337);
+        $hash = new HmacHash('md5', '1337');
         $hash->push('Hello World');
         $this->assertEquals('53d98cf0aa1b21b7fc2c676a86106221', $hash);
         $this->assertEquals(hash_hmac('md5', 'Hello World', 1337), $hash);
     }
 
     public function testSha1() {
-        $hash = new HmacHash('sha1', 1337);
+        $hash = new HmacHash('sha1', '1337');
         $hash->push('Hello World');
+
         $this->assertEquals('8f37db8db373c2dc9990873c8d7b72c92bfb201e', $hash);
-        $this->assertEquals(hash_hmac('sha1', 'Hello World', 1337), $hash);
+        $this->assertEquals(hash_hmac('sha1', 'Hello World', '1337'), $hash);
     }
 
     public function testClone() {
-        $foo = new HmacHash('sha1', 1337);
+        $foo = new HmacHash('sha1', '1337');
         $foo->push('foo');
 
         $foobar = clone $foo;
